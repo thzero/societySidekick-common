8a9ab09544ef8f64023ebda0484181c32ae1c812
thzero <thzero@users.noreply.github.com>
2021-01-18 14:02:33 -0600

    remove unneeded

--- data/class.js
+++ data/class.js
@@ -4,9 +4,7 @@ class ClassData extends GameSystemData {
 	constructor() {
 		super();
 
-		this.bulk = null;
 		this.category = null;
-		this.cost = null;
 		this.level = null;
 		this.categorySecondary = null;
 		this.categoryTertiary = null;
@@ -18,9 +16,7 @@ class ClassData extends GameSystemData {
 		if (!requested)
 			return;
 
-		this.bulk = requested.bulk;
 		this.category = requested.category;
-		this.cost = requested.cost;
 		this.level = requested.level;
 		this.categorySecondary = requested.categorySecondary;
 		this.categoryTertiary = requested.categoryTertiary;
