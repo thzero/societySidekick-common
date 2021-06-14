1cc20bf4300b8eb70127b816a12a55ecade7a851
thzero <corvette@thzero.com>
2021-06-14 11:42:49 -0500

    listing style

--- data/settingsScenario.js
+++ data/settingsScenario.js
@@ -2,6 +2,7 @@ class SettingsScenario {
 	constructor() {
 		this.id = null;
 		this.additional = null;
+		this.listingSytleFilter = null;
 		this.seasonFilter = null;
 		this.sortBy = null;
 		this.sortDirection = null;
