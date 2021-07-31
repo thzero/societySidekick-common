471c3b5574b1d7e0a125ad5e4cfd2be73ee5e59b
thzero <corvette@thzero.com>
2021-07-31 07:26:35 -0500

    missing fields

--- data/gameSystem.js
+++ data/gameSystem.js
@@ -4,8 +4,13 @@ class GameSystemData extends NamedData {
 	constructor() {
 		super();
 
+		this.active = true;
 		this.description = null;
+		this.family = null;
+		this.friendlyId = null;
 		this.gameSystemId = null;
+		this.genius = null;
+		this.species = null;
 		this.url = null;
 	}
 
@@ -13,8 +18,13 @@ class GameSystemData extends NamedData {
 		if (!requested)
 			return;
 
+		this.active = requested.active;
 		this.description = requested.description;
+		this.family = requested.family;
+		this.friendlyId = requested.friendlyId;
 		this.gameSystemId = requested.gameSystemId;
+		this.genius = requested.genius;
+		this.species = requested.species;
 		this.url = requested.url;
 	}
 }
